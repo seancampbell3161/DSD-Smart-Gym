422ba1513e2d21453a13fd04d4ad074dbec19e1c
drosa93 <DM_delarosa93@yahoo.com>
2025-07-15 10:04:44 -0500

    first classes commit

--- src/pages/Classes.tsx
+++ src/pages/Classes.tsx
@@ -1,9 +1,66 @@
+import React, {useState} from 'react';
+import ClassList from "../components/Dashboard/ClassList";
+import cycling from '../assets/cycling-class.jpg';
+import boxing from '../assets/boxing-class.jpg';
+
+
 const Classes: React.FC = () => {
-  // Footer logic here
+
+  const classList = [
+    {
+    name: 'clycling',
+    type: 'bike Ride',
+    description: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+    img: cycling
+  },
+   {
+    name: 'boxing',
+    type: 'bike Ride',
+    description: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+    img: boxing
+  },
+   {
+    name: 'clycling',
+    type: 'bike Ride',
+    description: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+    img: cycling
+  },
+   {
+    name: 'clycling',
+    type: 'bike Ride',
+    description: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+    img: cycling
+  },
+   {
+    name: 'clycling',
+    type: 'bike Ride',
+    description: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
+    img: cycling
+  },
+]
+
+const [backgroundImage, setBackgroundImage] = useState<string | null>(null);
+  
+// Footer logic here
+
+
   return (
-    <Classes>
+    <div
+      className='classes-page'
+      style={{
+        backgroundImage: backgroundImage ? `url(${backgroundImage})`: `none`,
+        backgroundSize: "cover",
+        backgroundPosition: "top",
+        transition: "background-image 0.3s ease",
+        minHeight: "100vh",
+      }}
+    >
+      <ClassList
+      classes={classList}
+      setBackgroundImg={setBackgroundImage}
+      />
       {/* Footer content */}
-    </Classes>
+    </div>
   );
 };
 
