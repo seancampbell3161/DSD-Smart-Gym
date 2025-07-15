c13f19c61a554e1f3415e269dfee6cdb94446cae
drosa93 <DM_delarosa93@yahoo.com>
2025-07-15 14:23:42 -0500

    added styling to the classes page

--- src/pages/Classes.tsx
+++ src/pages/Classes.tsx
@@ -1,7 +1,11 @@
 import React, {useState} from 'react';
 import ClassList from "../components/Dashboard/ClassList";
-import cycling from '../assets/cycling-class.jpg';
+import cycling from '../assets/cycling-class.png';
 import boxing from '../assets/boxing-class.jpg';
+import yoga from '../assets/yoga-class.png';
+import hiit from '../assets/hiit-class.png';
+import strength from '../assets/strength-training.png';
+import gym from '../assets/gym.png';
 
 
 const Classes: React.FC = () => {
@@ -20,26 +24,26 @@ const Classes: React.FC = () => {
     img: boxing
   },
    {
-    name: 'clycling',
+    name: 'yoga',
     type: 'bike Ride',
     description: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
-    img: cycling
+    img: yoga
   },
    {
-    name: 'clycling',
-    type: 'bike Ride',
+    name: 'HIIT',
+    type: 'High Intensity interval training',
     description: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
-    img: cycling
+    img: hiit
   },
    {
-    name: 'clycling',
-    type: 'bike Ride',
+    name: 'strength training',
+    type: 'resistance based weight training',
     description: 'Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups.',
-    img: cycling
+    img: strength
   },
 ]
 
-const [backgroundImage, setBackgroundImage] = useState<string | null>(null);
+const [backgroundImage, setBackgroundImage] = useState<string | null>(gym);
   
 // Footer logic here
 
@@ -48,7 +52,7 @@ const [backgroundImage, setBackgroundImage] = useState<string | null>(null);
     <div
       className='classes-page'
       style={{
-        backgroundImage: backgroundImage ? `url(${backgroundImage})`: `none`,
+        backgroundImage: backgroundImage ? `url(${backgroundImage})`: gym ,
         backgroundSize: "cover",
         backgroundPosition: "top",
         transition: "background-image 0.3s ease",
