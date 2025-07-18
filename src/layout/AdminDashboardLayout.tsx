7c6c42b284d3de7f37727a7784acb906d6ba6d22
EngineerMoore <alexiashalise@gmail.com>
2025-07-18 12:54:01 -0500

    rendered analytics page on dashboard and gathered tools and requirements

--- src/layout/AdminDashboardLayout.tsx
+++ src/layout/AdminDashboardLayout.tsx
@@ -2,8 +2,13 @@ interface AdminDashboardProps {
   children: React.ReactElement;
 }
 
-const AdminDashboardLayout: React.FC<AdminDashboardProps> = () => {
-  return <h2>Dashboard</h2>;
+const AdminDashboardLayout: React.FC<AdminDashboardProps> = ({children}) => {
+  return (
+    <>
+    <h2>Dashboard</h2>
+    {children}
+    </>
+  );
 };
 
 export default AdminDashboardLayout;
