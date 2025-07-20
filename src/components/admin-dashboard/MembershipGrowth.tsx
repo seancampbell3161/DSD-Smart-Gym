fbe82e5c7526f4e772a39845e7d264a2b55fd3db
EngineerMoore <alexiashalise@gmail.com>
2025-07-20 08:49:53 -0500

    designed time period filter to highlight the selected period and update the state accordingly

--- src/components/admin-dashboard/MembershipGrowth.tsx
+++ src/components/admin-dashboard/MembershipGrowth.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+import type { TimeOptions } from "../../types/TimeOptions";
 import TimePeriodButtons from "./TimePeriodButtons";
 import MetricCard from "./MetricCard";
 
 const MembershipGrowth: React.FC = () => {
+  const [timePeriod, setTimePeriod] = useState<TimeOptions>("monthly");
+
   return (
     <>
-      <TimePeriodButtons />
+      <TimePeriodButtons
+        timePeriod={timePeriod}
+        setTimePeriod={setTimePeriod}
+      />
       <MetricCard />
     </>
-  )
-}
+  );
+};
 export default MembershipGrowth;
--- src/components/admin-dashboard/TimePeriodButtons.tsx
+++ src/components/admin-dashboard/TimePeriodButtons.tsx
@@ -1,14 +1,42 @@
-import Button from 'react-bootstrap/Button';
-import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Button from "react-bootstrap/Button";
+import ButtonGroup from "react-bootstrap/ButtonGroup";
+import "../../styles/TimePeriodButtons.css";
+import type { TimeOptions } from "../../types/TimeOptions";
 
-const TimePeriodButtons: React.FC = () => {
+interface TimePeriodButtonProps {
+  timePeriod: TimeOptions;
+  setTimePeriod: React.Dispatch<React.SetStateAction<TimeOptions>>;
+}
+
+const TimePeriodButtons: React.FC<TimePeriodButtonProps> = ({
+  timePeriod,
+  setTimePeriod,
+}) => {
   return (
     <ButtonGroup aria-label="Basic example">
-      <Button variant="secondary">Yearly</Button>
-      <Button variant="secondary">Monthly</Button>
-      <Button variant="secondary">Weekly</Button>
+      <Button
+        className={timePeriod === "yearly" ? "selected" : ""}
+        variant="secondary"
+        onClick={() => setTimePeriod("yearly")}
+      >
+        Yearly
+      </Button>
+      <Button
+        className={timePeriod === "monthly" ? "selected" : ""}
+        variant="secondary"
+        onClick={() => setTimePeriod("monthly")}
+      >
+        Monthly
+      </Button>
+      <Button
+        className={timePeriod === "weekly" ? "selected" : ""}
+        variant="secondary"
+        onClick={() => setTimePeriod("weekly")}
+      >
+        Weekly
+      </Button>
     </ButtonGroup>
-  )
-}
+  );
+};
 
 export default TimePeriodButtons;
