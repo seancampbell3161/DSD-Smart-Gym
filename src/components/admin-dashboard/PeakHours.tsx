234809203f9cfbbb33ca0caba13575fa39a501e6
EngineerMoore <alexiashalise@gmail.com>
2025-08-02 16:38:50 -0500

    mounted the yearly range component to the peak hours card

--- src/components/admin-dashboard/PeakHours.tsx
+++ src/components/admin-dashboard/PeakHours.tsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
 import PeakHoursMap from "./PeakHoursMap";
 import "../../styles/PeakHours.css";
+import YearlyRange from "./YearlyRange";
 
 const PeakHours: React.FC = () => {
+  const [invalidYearFormat, setInvalidYearFormat] = useState<boolean>(false);
+
+  const pattern: RegExp = /^\d{4}$/;
+
   return (
     <div className="peak-hours-container">
       <h2 id="peak-hours-title">Peak Hours</h2>
+      <div className="yearly-range-container">
+        <YearlyRange
+          pattern={pattern}
+          setInvalidYearFormat={setInvalidYearFormat}
+        />
+        <div className={invalidYearFormat ? "invalid-year" : ""}>
+          {invalidYearFormat ? "Please enter valid year(YYYY)" : ""}
+        </div>
+      </div>
       <PeakHoursMap />
     </div>
   );
