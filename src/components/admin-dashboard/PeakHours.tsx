843f6dacf0717567ae56af0b71de2361fd81f280
EngineerMoore <alexiashalise@gmail.com>
2025-08-07 01:52:18 -0500

    BTS=16-timeframeViews replaced yearly reange on peak hours metric  with button group

--- src/components/admin-dashboard/PeakHours.tsx
+++ src/components/admin-dashboard/PeakHours.tsx
@@ -1,24 +1,23 @@
 import { useState } from "react";
 import PeakHoursMap from "./PeakHoursMap";
+import TimePeriodButtons from "./TimePeriodButtons";
+import type { TimeOptions } from "../../types/Analytics.interface";
 import "../../styles/PeakHours.css";
-import YearlyRange from "./YearlyRange";
 
 const PeakHours: React.FC = () => {
-  const [invalidYearFormat, setInvalidYearFormat] = useState<boolean>(false);
-
-  const pattern: RegExp = /^\d{4}$/;
+  const [timePeriod, setTimePeriod] = useState<TimeOptions>({
+    button: "Monthly",
+    tableHeader: "Month",
+  });
 
   return (
     <div className="peak-hours-container">
       <h2 id="peak-hours-title">Peak Hours</h2>
-      <div className="yearly-range-container">
-        <YearlyRange
-          pattern={pattern}
-          setInvalidYearFormat={setInvalidYearFormat}
+      <div className="time-period-buttons">
+        <TimePeriodButtons
+        timePeriod={timePeriod}
+        setTimePeriod={setTimePeriod}
         />
-        <div className={invalidYearFormat ? "invalid-year" : ""}>
-          {invalidYearFormat ? "Please enter valid year(YYYY)" : ""}
-        </div>
       </div>
       <PeakHoursMap />
     </div>
